dcc3659111c7fb920fbaf18bc1dcd94cddc8326f
RushilYadav <rushilyadav18@gmail.com>
2025-09-02 13:45:19 +0100

    Updated db.js with .env import for privacy

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'postgres',       // your postgres user
-  host: 'localhost',      // local machine
-  database: 'SmartRoll', // the database you just created
-  password: 'Rushkush180206!', // the postgres password
-  port: 5432,             // default
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
